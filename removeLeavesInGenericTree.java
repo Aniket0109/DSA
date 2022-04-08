b18421f44315d01fd0fd3d972019c1c112a3bc34
Aniket <kumaraniket0109@gmail.com>
2022-04-08 23:26:24 +0530

    Completed

--- removeLeavesInGenericTree.java
+++ removeLeavesInGenericTree.java
@@ -136,11 +136,16 @@ public class removeLeavesInGenericTree {
 
     public static void removeLeaves(Node node) {
 
-        if (node.children.isEmpty()) {
+        for(int i=node.children.size()-1; i>=0;i--){
 
-            node = new Node();
+            Node child = node.children.get(i);
+            if(child.children.size()==0){
 
+                node.children.remove(child);
+
+            }
         }
+
         for(Node child:node.children) {
 
             removeLeaves(child);
