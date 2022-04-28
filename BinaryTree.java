9d53995dee1383f4770fe84a9d32f693664493f9
Aniket <kumaraniket0109@gmail.com>
2022-04-28 13:49:15 +0530

    Completed

--- BinaryTree.java
+++ BinaryTree.java
@@ -35,7 +35,7 @@ public class BinaryTree {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
 
         Integer[] arr = {50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
         Node root = new Node(arr[0], null, null);
@@ -50,13 +50,33 @@ public class BinaryTree {
 
             if(top.state==1){
 
-                st.push(new Pair(top.node.left, 1));
+                idx++;
+                if(arr[idx] !=null){
+
+                    top.node.left = new Node(arr[idx], null, null);
+                    st.push(new Pair(top.node.left, 1));
+
+                } else {
+
+                    top.node.left = null;
+
+                }
                 top.state++;
 
             } else if(top.state==2){
 
-                st.push(new Pair(top.node.right, 1));
-                top.state--;
+                idx++;
+                if(arr[idx] !=null){
+
+                    top.node.right = new Node(arr[idx], null, null);
+                    st.push(new Pair(top.node.right, 1));
+
+                } else {
+
+                    top.node.right = null;
+
+                }
+                top.state++;
 
             } else {
 
