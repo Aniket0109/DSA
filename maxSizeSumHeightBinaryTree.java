aa5d983bccc2af2c005a78d0c8c4edce270f9ae9
Aniket <kumaraniket0109@gmail.com>
2022-04-29 13:28:18 +0530

    Completed

--- maxSizeSumHeightBinaryTree.java
+++ maxSizeSumHeightBinaryTree.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class maxSizeSumBinaryTree {
+public class maxSizeSumHeightBinaryTree {
 
     public static int size(BinaryTree.Node node) {
         int s=0;
