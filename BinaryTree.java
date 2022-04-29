a17f0731b104d22165789ed87882f6c20d02af0c
Aniket <kumaraniket0109@gmail.com>
2022-04-29 13:08:22 +0530

    Completed

--- BinaryTree.java
+++ BinaryTree.java
@@ -30,55 +30,50 @@ public class BinaryTree {
         }
     }
 
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<BinaryTree.Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            BinaryTree.Pair top = st.peek();
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new BinaryTree.Node(arr[idx], null, null);
+                    BinaryTree.Pair lp = new BinaryTree.Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new BinaryTree.Node(arr[idx], null, null);
+                    BinaryTree.Pair rp = new BinaryTree.Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) throws Exception{
 
         Integer[] arr = {50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
-        Node root = new Node(arr[0], null, null);
-        Pair rootPair = new Pair(root, 1);
-        Stack<Pair> st = new Stack<>();
-        st.push(rootPair);
-
-        int idx =0;
-        while(st.size()>0){
-
-            Pair top = st.peek();
-
-            if(top.state==1){
-
-                idx++;
-                if(arr[idx] !=null){
-
-                    top.node.left = new Node(arr[idx], null, null);
-                    st.push(new Pair(top.node.left, 1));
-
-                } else {
-
-                    top.node.left = null;
-
-                }
-                top.state++;
-
-            } else if(top.state==2){
-
-                idx++;
-                if(arr[idx] !=null){
-
-                    top.node.right = new Node(arr[idx], null, null);
-                    st.push(new Pair(top.node.right, 1));
-
-                } else {
-
-                    top.node.right = null;
-
-                }
-                top.state++;
-
-            } else {
-
-                st.pop();
-
-            }
-        }
+        Node root = construct(arr);
         displayBinaryTree.displayBinaryT(root);
     }
 }
