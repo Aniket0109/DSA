38283b76b94b42e20c7bb7f6672bea2c7c0ae071
Aniket <kumaraniket0109@gmail.com>
2022-05-23 09:53:40 +0530

    Completed

---
+++ highestFrequencyCharacter.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
+public class highestFrequencyCharacter {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner((System.in));
+        String s = sc.next();
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for(int i=0; i<s.length();i++){
+
+            if(hm.get(s.charAt(i))!=null) {
+                hm.put(s.charAt(i), hm.get(s.charAt(i)) + 1);
+            } else {
+                hm.put(s.charAt(i), 1);
+            }
+        }
+
+        Set<Character> keys = hm.keySet();
+        int max = Integer.MIN_VALUE;
+        char ans = 'z';
+        for(char key : keys){
+
+            int val = hm.get(key);
+            if(max<val){
+                ans = key;
+            }
+            max = Math.max(val, max);
+
+        }
+
+        System.out.println(ans);
+
+    }
+
+}
