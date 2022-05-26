f27cb8f3c06a515fd2d2191c074dbbdf1ac1e9bf
Aniket <kumaraniket0109@gmail.com>
2022-05-26 18:55:48 +0530

    Completed

--- sortANearlySortedArray.java
+++ sortANearlySortedArray.java
@@ -12,12 +12,27 @@ public class sortANearlySortedArray {
         for (int i = 0; i < n; i++) {
 
             a[i] = sc.nextInt();
+//            pq.add(a[i]);
+        }
+        int k = sc.nextInt();
+
+        for(int i=0; i<=k; i++){
+
             pq.add(a[i]);
+
+        }
+
+        for(int i = k+1; i< a.length; i++){
+
+            System.out.println(pq.remove());
+            pq.add(a[i]);
+
         }
 
         while(pq.size()>0){
 
             System.out.println(pq.remove());
+
         }
     }
 }
