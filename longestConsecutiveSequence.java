1fc4516664e576dc78734d391ba5a0f603b74725
Aniket <kumaraniket0109@gmail.com>
2022-05-26 09:05:15 +0530

    Completed

---
+++ longestConsecutiveSequence.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class longestConsecutiveSequence {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int[] a1 = new int[n1];
+
+        for (int i = 0; i < n1; i++) {
+
+            a1[i] = sc.nextInt();
+
+        }
+    }
+}
