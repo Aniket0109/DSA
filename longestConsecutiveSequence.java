9e786d666ce78ad9124c12ce967d2ad73daa4c04
Aniket <kumaraniket0109@gmail.com>
2022-05-26 10:03:17 +0530

    Completed

--- longestConsecutiveSequence.java
+++ longestConsecutiveSequence.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class longestConsecutiveSequence {
@@ -6,11 +7,50 @@ public class longestConsecutiveSequence {
 
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
-        int[] a1 = new int[n1];
+        int[] a = new int[n1];
 
+        HashMap<Integer, Boolean> hm = new HashMap<>();
         for (int i = 0; i < n1; i++) {
 
-            a1[i] = sc.nextInt();
+            a[i] = sc.nextInt();
+            hm.put(a[i], true);
+
+        }
+
+        for(int val : a){
+
+            if(hm.containsKey(val-1)){
+                hm.put(val, false);
+            }
+
+        }
+
+        int msp = 0;
+        int ml = 0;
+
+        for(int val : a){
+
+            if(hm.get(val)){
+                int tl = 1;
+                int tsp = val;
+
+                while(hm.containsKey(tsp+tl)){
+                    tl++;
+                }
+
+                if(tl>ml){
+
+                    msp = tsp;
+                    ml = tl;
+
+                }
+
+            }
+        }
+
+        for(int i=0; i<ml; i++){
+
+            System.out.println(msp+i);
 
         }
     }
