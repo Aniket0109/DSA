86b12bcd2643ca419cf676abdc8a76bf66823974
Aniket <kumaraniket0109@gmail.com>
2022-07-07 22:33:24 +0530

    Completed

---
+++ partitionAnArray.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class partitionAnArray {
+
+    public static void partition(int[] arr, int pivot){
+        int i=0, j=0;
+        while(i<arr.length){
+            if(arr[i]>pivot)
+                i++;
+            else {
+                swap(arr, i, j);
+                i++;
+                j++;
+            }
+        }
+    }
+
+    // used for swapping ith and jth elements of array
+    public static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void print(int[] arr) {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int pivot = scn.nextInt();
+        partition(arr,pivot);
+        print(arr);
+    }
+}
